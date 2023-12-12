31a488d9cfe18eaabbb5d50942d3606ff3e21f04
zDox <zdox@artixus.de>
2023-12-12 22:13:48 +0100

    Working fill.asm

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,75 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 //// Replace this comment with your code.
+// init variables
+// Load 8192 into pixels
+(BEGIN)
+@8192
+D = A
+@pixels
+M = D
+
+@i
+M = 0
+
+// if key not pressed jump to color screen black
+@KBD
+D = M
+@BLACK
+D; JGT
+
+// -- make screen white --
+
+(WHITE)
+// if is above screen goto END
+@pixels
+D = M
+@i
+D = M - D
+@END
+D; JGE
+
+// make ith pixel white
+@SCREEN
+D = A
+@i
+A = D + M
+M = 0
+
+// i = i + 1
+@i
+M = M + 1
+
+// Jump to begin of white loop
+@WHITE
+0;JMP
+
+// else make screen black
+(BLACK)
+
+// if is above screen goto END
+@pixels
+D = M
+@i
+D = M - D
+@END
+D; JGE
+
+// make ith pixel black
+@SCREEN
+D = A
+@i
+A = D + M
+M = -1
+
+// i = i + 1
+@i
+M = M + 1
+
+// Jump to begin of black loop
+@BLACK
+0;JMP
+
+(END)
+@BEGIN
+0; JMP
