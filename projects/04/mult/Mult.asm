ed0a5adf32c27e4933b8486d940e18a3f68c6c53
zDox <zdox@artixus.de>
2023-12-12 21:34:11 +0100

    Small fixes to old files and project 04 multiplication

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -7,4 +7,37 @@
 // Assumes that R0 >= 0, R1 >= 0, and R0 * R1 < 32768.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-//// Replace this comment with your code.
+// Approach: Add RAM[1] many times RAM[0] to RAM[2]
+
+// Init of variables
+@i
+M = 0
+@R2
+M = 0
+
+(LOOP)
+    // if i >= RAM[1] goto END
+    @R1
+    D = M
+    @i
+    D = M - D 
+    @END
+    D;JGE
+
+    // Add RAM[0] to RAM[2]
+    @R0
+    D = M
+    @R2
+    M = D + M
+
+    // i = i + 1
+    @i
+    M = M + 1
+
+    // goto LOOP
+    @LOOP
+    0; JMP
+
+(END)
+    @END
+    0; JMP
