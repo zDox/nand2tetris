807ceabab9e37b52a2ef3fc8bc80b82515062978
zDox <zdox@artixus.de>
2023-12-22 23:30:20 +0100

    Started work on Project08

---
+++ projects/08/vm_translator/src/main.rs
@@ -0,0 +1,19 @@
+
+use std::{ env, path::Path };
+
+mod vm_translator;
+use vm_translator::VMTranslator;
+
+fn main() {    
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        return;
+    }
+    let path_arg = &args[1];
+
+    let path = Path::new(&path_arg);
+    let mut vm_translator = VMTranslator::new(&path);
+    vm_translator.translate();
+
+    println!("Translation of file '{}' completed", path.file_name().expect("Not a Ostr").to_str().unwrap_or(""));
+}
